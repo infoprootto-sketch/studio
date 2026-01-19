30a27246157ce30f00bf8b0ce729dc27364f9767
Prootto India <info.prootto@gmail.com>
2026-01-19 08:02:08 +0000

    I want you to run command to remove the sections which are not currently

--- src/lib/demo-data.ts
+++
@@ -1,5 +0,0 @@
-
-// This file is intentionally left empty.
-// In a real application, this file would be populated with mock data
-// for the interactive demo experience.
-// For the purpose of fixing the build, its existence is what matters.
